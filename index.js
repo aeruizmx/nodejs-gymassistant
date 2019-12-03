71f65b9381eb073c79366a1f9be4c5b6f83450cf
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-12-03 13:17:23 -0600

    clase 2

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.jsonp());
 
 server.post('/', (req, res, next)=> {
-    console.log(req.body);
+    let queryResult = req.body.queryResult;
+    let exerciseType = queryResult.parameters.exerciseType;
+    let fulfillmentText = queryResult.fulfillmentText;
+    let exercises = ['pushups', 'pullups', 'benchpress', 'legpress'];
+    let found = exercises.findIndex((exercise) => exercise == exerciseType);
+   if(found== (exercises.length - 1)) {
+       res.json({
+           "fulfillmentText": "Okay, that's enough for today, get some rest!"
+       });
+   }else{
+    res.json({
+        "fulfillmentText": fulfillmentText.replace('_exerciseType', exercises[found+1])
+    });
+   }
     return next();
 });
 server.listen(PORT, () => console.log(`Server is listening on port: ${PORT} `));
